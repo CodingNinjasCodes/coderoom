1fcb2487167372ed84f7d287953ae6058f29efce
arpansac <arpansac@gmail.com>
2020-02-24 17:11:18 +0530

    add config for firebase

--- src/index.js
+++ src/index.js
@@ -5,11 +5,18 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import * as firebase from 'firebase';
 
-config = {
+const firebaseConfig = {
+    apiKey: "AIzaSyCyg3-TiRdd_8qP1q3TwY7EA_9iPOPpbE4",
+    authDomain: "coderoom-dev.firebaseapp.com",
+    databaseURL: "https://coderoom-dev.firebaseio.com",
+    projectId: "coderoom-dev",
+    storageBucket: "coderoom-dev.appspot.com",
+    messagingSenderId: "694801682636",
+    appId: "1:694801682636:web:1f0b9ed0900cdd15a8f269",
+    measurementId: "G-2S2MQSLD75"
+  };
 
-}
-
-firebase.initializeApp(config);
+firebase.initializeApp(firebaseConfig);
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
